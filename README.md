5258261b0200e81e283fe6053484f3ece54368b3
Robert Soriano <sorianorobertc@gmail.com>
2022-06-28 22:37:56 -0700

    fix: incorrect export names

--- README.md
+++ README.md
@@ -19,12 +19,15 @@ import { createSpring, animated } from 'solid-spring'
 import { useDrag } from 'solid-gesture'
 
 function PullRelease() {
-  const styles = createSpring({ x: 0, y: 0 })
-
-  // Set the drag hook and define component movement based on gesture data
-  const bind = useDrag(({ down, movement: [mx, my] }) => {
-    api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down })
+  const [coords, setCoords] = createSignal({
+    x: 0,
+    y: 0
   })
+
+  const styles = createSpring(() => ({
+    x: coords().x,
+    y: coords().y 
+  }))
 
   // Bind it to a component
   return <animated.div {...bind()} style={styles()} />
--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,27 +1,26 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, createSignal } from 'solid-js';
+import { createSpring, animated } from 'solid-spring'
+import { useDrag } from 'solid-gesture'
+import './App.css'
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+  const [coords, setCoords] = createSignal({
+    x: 0,
+    y: 0
+  })
+
+  const styles = createSpring(() => ({
+    x: coords().x,
+    y: coords().y 
+  }))
+
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    setCoords({ x: down ? mx : 0, y: down ? my : 0 })
+  })
+
+  // Bind it to a component
+  return <animated.div class="box" {...bind()} style={styles()} />
+}
 
 export default App;
